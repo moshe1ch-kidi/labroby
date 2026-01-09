a846db240c9bade92262896c9490078f0a4f8b7a
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-01-09 18:10:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // השורה שמתחת היא הקריטית לפתרון המסך הלבן ב-GitHub Pages
+  // השורה הזו פותרת את שגיאת ה-404 ב-GitHub Pages
   base: '/labroby/',
   build: {
-    // הגדרה שעוזרת למנוע שגיאות זיכרון ב-Build של ספריות כבדות כמו Three.js
     chunkSizeWarningLimit: 1600,
   }
 })
