22a1495ad57a7f848141459d8a9ccaef3d495154
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-01-09 18:06:49 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // השורה שמתחת היא הקריטית לפתרון המסך הלבן ב-GitHub Pages
+  base: '/labroby/',
+  build: {
+    // הגדרה שעוזרת למנוע שגיאות זיכרון ב-Build של ספריות כבדות כמו Three.js
+    chunkSizeWarningLimit: 1600,
+  }
 })
