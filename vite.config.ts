c41979579a31e61a6232cf567e377ed16049bd6b
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-01-09 16:39:32 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/labroby/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // מעלה את רף האזהרה
+    rollupOptions: {
+      output: {
+        // פיצול ספריות חיצוניות לקבצים נפרדים כדי להקל על הטעינה
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
 })
