0a3ea8e6f65001aaf16e9779ce9a1402b70a8a55
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-01-09 17:22:55 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-
+ 
 import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Line } from '@react-three/drei';
