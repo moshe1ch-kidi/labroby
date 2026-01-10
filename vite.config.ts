4ecdf05c790cca3dd1d4355fb15c20c9603df1b9
moshe1ch-kidi <moshe1.ch@gmail.com>
2026-01-10 07:36:39 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // השורה הזו פותרת את שגיאת ה-404 ב-GitHub Pages
-  base: '/labroby/',
-  build: {
-    chunkSizeWarningLimit: 1600,
-  }
 })
